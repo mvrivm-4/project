8a8bbb1650b982eee7e1718b3258ca07c598bd77
mariam <mrwanmohamd206@gmail.com>
2025-02-27 19:36:45 +0200

    Add files via upload

---
+++ protfilo.js
@@ -0,0 +1,25 @@
+let menu = document.querySelector('#menu-icon')
+let navbar = document.querySelector(".navbar")
+let section = document.querySelectorAll("section")
+let link = document.querySelectorAll(".header nav a")
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            link.forEach(lin => {
+                lin.classList.remove("active")
+                document.querySelector('header nav a [href *=' + id + ']').classList.add("active")
+            })
+
+        }
+    })
+}
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle("active")
+
+}
